1b438d7eb97ed9aedf518341aa7e6df530f2c782
Arjun <arjun.kumar@quokkalabs.com>
2025-08-12 16:44:29 +0530

    initial commit

---
+++ models/note.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  tags: { type: [String], required: false },
+}, {
+  timestamps: true,
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+module.exports = Note;
